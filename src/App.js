b9763e9b8281915fd85aa53e4e7ed2532a5c6d28
Rhys-W-B <rhys96623@gmail.com>
2023-09-23 18:19:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+/* global BigInt */
 
 function App() {
+  const [microcount, setmicroCount] = useState(0);
+  const [millicount, setmilliCount] = useState(0);
+  const [measurement, setMeasurement] = useState("micrometers");
+  const [displayVal, setDisplayVal] = useState(0);
+
+  const calcMeasurement = () => {};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="images/Tumorbuddy.png"
+          className="tumorBuddy"
+          alt="logo"
+          onClick={calcMeasurement}
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {measurement} Covered: {displayVal}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
